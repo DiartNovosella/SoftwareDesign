290d9ce0f6fa3e4956649943d07e815400c1ad40
DiartNovosella <47432878+DiartNovosella@users.noreply.github.com>
2019-02-07 23:04:24 +0100

    ListOfStudent For StudentView

---
+++ eLearn_v2/Models/ListofStudents/StudentIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eLearn_v1.Models.ListofStudents
+{
+    public class StudentIndexViewModel
+    {
+        public List<Student> Students { get; set; }
+    }
+}
