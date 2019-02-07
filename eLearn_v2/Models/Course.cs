774cfe2234678a264cc5ca002840e25106e1ecea
Diart Novosella <diart.aa@gmail.com>
2019-02-07 22:55:49 +0100

    InitialCommit

---
+++ eLearn_v2/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eLearn_v2.Models
+{
+    public class Course
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
