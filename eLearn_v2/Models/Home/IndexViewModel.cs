303540c53d2743c4ce45e7f123299c26d158111e
DiellorHoxhaj <diellorhoxhaj@gmail.com>
2019-02-07 23:03:49 +0100

    Home Model

---
+++ eLearn_v2/Models/Home/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models.Home
+{
+    public class IndexViewModel
+    {
+        public List<Course> Courses { get; set; }
+    }
+}
