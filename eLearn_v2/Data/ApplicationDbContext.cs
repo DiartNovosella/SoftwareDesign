0a128b80de91d19a7550e3b9c832ceea7952001e
ValmaHasani <47418799+ValmaHasani@users.noreply.github.com>
2019-02-07 23:02:06 +0100

    ApplicationDBContext + SeedData
    Seed Data Changes Authorization if admin or normal user.

--- eLearn_v2/Data/ApplicationDbContext.cs
+++ eLearn_v2/Data/ApplicationDbContext.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+using eLearn_v1.Models;
 
-namespace eLearn_v2.Data
+namespace WebApplication1.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
@@ -12,5 +14,60 @@ namespace eLearn_v2.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            #region CourseSeed
+
+            modelBuilder.Entity<Course>().HasData(new Course
+            {
+                Id = 1,
+                Name = "Introduction To Programming",
+                Description = "This course is about programming languages and bascis in Fundamental ....",
+                Rating = 4,
+                Image="programming.png"
+          
+            });
+
+            modelBuilder.Entity<Course>().HasData(new Course
+            {
+                Id = 2,
+                Name = "Introduction To Programming",
+                Description = "This course is about programming languages and bascis in Fundamental ....",
+                Rating = 10,
+                Image = "programming.png"
+
+
+            });
+            modelBuilder.Entity<Course>().HasData(new Course
+            {
+                Id = 3,
+                Name = "Introduction To Programming",
+                Description = "This course is about programming languages and bascis in Fundamental ....",
+                Rating = 2,
+                Image = "programming.png"
+
+            });
+            modelBuilder.Entity<Course>().HasData(new Course
+            {
+                Id = 4,
+                Name = "Introduction To Programming",
+                Description = "This course is about programming languages and bascis in Fundamental ....",
+                Rating = 7,
+                Image = "programming.png"
+
+
+            });
+
+
+            #endregion
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Student> Student { get; set; }
+       
     }
 }
---
+++ eLearn_v2/Data/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Data
+{
+    public class SeedData
+    {
+        public static void Seed(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+        }
+
+        private static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("NormalUser").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "NormalUser";
+                IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+            }
+
+
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "Administrator";
+                IdentityResult roleResult = roleManager.
+                CreateAsync(role).Result;
+            }
+        }
+
+        private static void SeedUsers(UserManager<IdentityUser> userManager)
+        {
+            if (userManager.FindByNameAsync("admin@localhost").Result == null)
+            {
+                IdentityUser user = new IdentityUser();
+                user.UserName = "admin@test.com";
+                user.Email = "admin@test.com";
+
+                IdentityResult result = userManager.CreateAsync(user, "Admin123!").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Administrator").Wait();
+                }
+            }
+
+        }
+    }
+}
